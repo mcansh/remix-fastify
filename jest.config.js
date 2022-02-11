319c2d351fbf0c4888eca8a15b3a8c5d07037fdb
Logan McAnsh <logan@mcan.sh>
2022-02-10 22:47:44 -0500

    chore: remove ts-jest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
-const path = require("path");
-
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
-  globals: {
-    "ts-jest": {
-      tsconfig: path.join(process.cwd(), "tsconfig.json"),
-      diagnostics: false,
-    },
-  },
 };
