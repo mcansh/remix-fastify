3f5b8b3405c94a31ff60569472b25c9bd9a0bb03
Logan McAnsh <logan@mcan.sh>
2022-05-20 12:27:05 -0400

    feat: build using esbuild
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ build.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+let { build, ts, tsconfig, dirname, glob, log } = require("estrella");
+let pkg = require("./package.json");
+
+build({
+  entry: glob("src/**/*.ts"),
+  outdir: dirname(pkg.main),
+  minify: false,
+  target: "node14",
+  format: "cjs",
+  platform: "node",
+  sourcemap: true,
+  onEnd(config) {
+    generateTypeDefs(tsconfig(config), config.entry, config.outdir);
+  },
+});
+
+function generateTypeDefs(tsconfig, entryfiles, outDir) {
+  let entries = Array.isArray(entryfiles) ? entryfiles : [entryfiles];
+  let allEntries = [...entries, ...tsconfig.include];
+  let uniqueEntries = [...new Set(allEntries)];
+  let filenames = uniqueEntries.filter((v) => v);
+
+  log.info("Generating type declaration files for", filenames.join(", "));
+  let compilerOptions = {
+    ...tsconfig.compilerOptions,
+    moduleResolution: undefined,
+    declaration: true,
+    outDir,
+  };
+  let program = ts.ts.createProgram(filenames, compilerOptions);
+  let targetSourceFile = undefined;
+  let writeFile = undefined;
+  let cancellationToken = undefined;
+  let emitOnlyDtsFiles = true;
+  program.emit(
+    targetSourceFile,
+    writeFile,
+    cancellationToken,
+    emitOnlyDtsFiles
+  );
+  log.info("Wrote", glob(outDir + "/*.d.ts").join(", "));
+}
