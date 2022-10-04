a85477d3a7e35c8f6de503af4bf0c615a63197a5
Logan McAnsh <logan@mcan.sh>
2022-10-03 20:50:48 -0700

    chore: update build script and add publish script
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ scripts/publish.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const path = require("node:path");
+const { execSync } = require("node:child_process");
+const semver = require("semver");
+const glob = require("glob");
+
+const packageDir = path.resolve(process.cwd(), "../packages");
+const packages = glob.sync("packages/*", { absolute: true });
+
+console.log({ packages });
+
+function getTaggedVersion() {
+  let output = execSync("git tag --list --points-at HEAD").toString().trim();
+  return output.replace(/^v/g, "");
+}
+
+/**
+ * @param {string} dir
+ * @param {string} tag
+ */
+function publish(dir, tag) {
+  execSync(`npm publish --access public --tag ${tag} ${dir}`, {
+    stdio: "inherit",
+  });
+}
+
+async function run() {
+  // Make sure there's a current tag
+  let taggedVersion = getTaggedVersion();
+  if (taggedVersion === "") {
+    console.error("Missing release version. Run the version script first.");
+    process.exit(1);
+  }
+
+  let prerelease = semver.prerelease(taggedVersion);
+  let prereleaseTag = prerelease ? String(prerelease[0]) : undefined;
+  let tag = prereleaseTag
+    ? prereleaseTag.includes("nightly")
+      ? "nightly"
+      : prereleaseTag.includes("experimental")
+      ? "experimental"
+      : prereleaseTag
+    : "latest";
+
+  // Publish all @remix-run/* packages
+  for (let name of packages) {
+    publish(name, tag);
+  }
+}
+
+run().then(
+  () => {
+    process.exit(0);
+  },
+  (error) => {
+    console.error(error);
+    process.exit(1);
+  }
+);
