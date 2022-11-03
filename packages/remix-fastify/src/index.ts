5e773471fb65593af8cf8e736fa113101f186985
Logan McAnsh <logan@mcan.sh>
2022-11-03 18:51:17 -0400

    feat: generate esm build
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-fastify/src/index.ts
+++ packages/remix-fastify/src/index.ts
@@ -1,5 +1,4 @@
 import "./globals";
-
 export type { GetLoadContextFunction, RequestHandler } from "./server";
 export { createRequestHandler } from "./server";
 export { remixFastifyPlugin } from "./plugin";
