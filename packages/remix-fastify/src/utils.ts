9337062dc111eb01b0d8cb85b07a2f96bc9083ea
Logan McAnsh <logan@mcan.sh>
2023-03-15 21:58:28 -0400

    fix: leading slash (#78)
    * fix: leading slash
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Create .changeset/stale-days-accept.md
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-fastify/src/utils.ts
+++ packages/remix-fastify/src/utils.ts
@@ -38,7 +38,7 @@ export function getStaticFiles({
     return {
       isBuildAsset,
       filePublicPath: filepath,
-      browserAssetUrl,
+      browserAssetUrl: `/${browserAssetUrl}`,
     };
   });
 }
