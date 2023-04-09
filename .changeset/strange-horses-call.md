d3c16fb3d5ed971d5e062445605914ff3e28860c
Logan McAnsh <logan@mcan.sh>
2023-04-09 14:52:50 -0400

    feat: add support for early hints (#74)
    * feat: add support for early hints
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: allow disabling earlyHints
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Create .changeset/strange-horses-call.md
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore(deps): make router a peer dependency
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: rename function
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: add comments to plugin options
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: mark early hints as unstable and remove default
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: resolving getLoadContext is already done in server
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .changeset/strange-horses-call.md
@@ -0,0 +1,5 @@
+---
+"@mcansh/remix-fastify": patch
+---
+
+feat: add support for early hints (HTTP 103)
--- packages/remix-fastify/src/plugin.ts
+++ packages/remix-fastify/src/plugin.ts
@@ -1,11 +1,14 @@
 import * as path from "node:path";
 import { pathToFileURL, URL } from "node:url";
+import type { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify";
 import fastifyStatic from "@fastify/static";
+import type { EarlyHintItem } from "@fastify/early-hints";
+import fastifyEarlyHints from "@fastify/early-hints";
 import type { ServerBuild } from "@remix-run/node";
-import type { FastifyPluginAsync, FastifyReply } from "fastify";
 import fp from "fastify-plugin";
 import fastifyRacing from "fastify-racing";
 import invariant from "tiny-invariant";
+import { matchRoutes } from "@remix-run/router";
 
 import type { GetLoadContextFunction } from "./server";
 import { createRequestHandler } from "./server";
@@ -13,14 +16,47 @@ import type { StaticFile } from "./utils";
 import { getStaticFiles, purgeRequireCache } from "./utils";
 
 interface PluginOptions {
+  /**
+   * The Remix build to use.
+   * This can either be a path to the build or the build itself.
+   */
   build?: ServerBuild | string;
+  /**
+   * The mode your app is running in.
+   * This is used to determine how to handle errors.
+   * @default process.env.NODE_ENV
+   */
   mode?: string;
+  /**
+   * The root directory of your app.
+   * This is used to resolve the `public` directory.
+   * @default process.cwd()
+   */
   rootDir?: string;
+  /**
+   * A function that returns the value to use as `context` in route `loader` and
+   * `action` functions.
+   *
+   * You can think of this as an escape hatch that allows you to pass
+   * environment/platform-specific values through to your loader/action.
+   */
   getLoadContext?: GetLoadContextFunction;
+  /**
+   * purge the require cache in development
+   * this should be disabled if you are using `unstable_dev`.
+   * @default process.env.NODE_ENV === "development"
+   */
   purgeRequireCacheInDevelopment?: boolean;
+  /**
+   * enable early hints
+   * note this won't include any imports from your remix `links()` function.
+   * it will only include the js needed to render the current page
+   * @default true
+   */
+  unstable_earlyHints?: boolean;
 }
 
-async function loadBuild(build: ServerBuild | string) {
+async function loadBuild(build: ServerBuild | string): Promise<ServerBuild> {
   if (typeof build === "string") {
     if (!build.endsWith(".js")) {
       build = path.join(build, "index.js");
@@ -43,6 +79,7 @@ let remixFastify: FastifyPluginAsync<PluginOptions> = async (
     mode = process.env.NODE_ENV,
     rootDir = process.cwd(),
     purgeRequireCacheInDevelopment = process.env.NODE_ENV === "development",
+    unstable_earlyHints: earlyHints,
   } = options;
   invariant(build, "You must provide a build");
   let serverBuild: ServerBuild = await loadBuild(build);
@@ -53,6 +90,9 @@ let remixFastify: FastifyPluginAsync<PluginOptions> = async (
     });
   }
 
+  if (earlyHints) {
+    fastify.register(fastifyEarlyHints, { warn: true });
+  }
   fastify.register(fastifyRacing, { handleError: true });
 
   let PUBLIC_DIR = path.join(rootDir, "public");
@@ -109,11 +149,6 @@ let remixFastify: FastifyPluginAsync<PluginOptions> = async (
     }
   }
 
-  let getLoadContext =
-    typeof options.getLoadContext === "function"
-      ? options.getLoadContext
-      : undefined;
-
   if (mode === "development" && typeof build === "string") {
     fastify.all("*", async (request, reply) => {
       invariant(build, "we lost the build");
@@ -124,21 +159,32 @@ let remixFastify: FastifyPluginAsync<PluginOptions> = async (
       if (purgeRequireCacheInDevelopment) {
         purgeRequireCache(build);
       }
+
+      let loaded = await loadBuild(build);
+
+      if (earlyHints) {
+        let links = getEarlyHintLinks(request, loaded);
+        await reply.writeEarlyHintsLinks(links);
+      }
+
       return createRequestHandler({
-        build: await loadBuild(build),
+        build: loaded,
         mode,
-        getLoadContext,
+        getLoadContext: options.getLoadContext,
       })(request, reply);
     });
   } else {
-    fastify.all(
-      "*",
+    fastify.all("*", async (request, reply) => {
+      if (earlyHints) {
+        let links = getEarlyHintLinks(request, serverBuild);
+        await reply.writeEarlyHintsLinks(links);
+      }
       createRequestHandler({
         build: serverBuild,
         mode,
-        getLoadContext,
-      })
-    );
+        getLoadContext: options.getLoadContext,
+      })(request, reply);
+    });
   }
 };
 
@@ -146,3 +192,31 @@ export let remixFastifyPlugin = fp(remixFastify, {
   name: "@mcansh/remix-fastify",
   fastify: "^3.29.0 || ^4.0.0",
 });
+
+function getEarlyHintLinks(
+  request: FastifyRequest,
+  serverBuild: ServerBuild
+): EarlyHintItem[] {
+  let origin = `${request.protocol}://${request.hostname}`;
+  let url = new URL(`${origin}${request.url}`);
+
+  let routes = Object.values(serverBuild.assets.routes);
+  let matches = matchRoutes(routes, url.pathname);
+  if (!matches || matches.length === 0) return [];
+  let links = matches.flatMap((match) => {
+    let routeImports = match.route.imports || [];
+    let imports = [
+      match.route.module,
+      ...routeImports,
+      serverBuild.assets.url,
+      serverBuild.assets.entry.module,
+      ...serverBuild.assets.entry.imports,
+    ];
+
+    return imports;
+  });
+
+  return links.map((link) => {
+    return { href: link, as: "script", rel: "preload" };
+  });
+}
