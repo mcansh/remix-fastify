86090e99276d9226facbf5735e576946a0f92583
Logan McAnsh <logan@mcan.sh>
2023-05-02 18:52:51 -0400

    chore: update example to call broadcastDevReady
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- example/server.js
+++ example/server.js
@@ -1,29 +1,26 @@
-let path = require("node:path");
 let fastify = require("fastify");
 let { remixFastifyPlugin } = require("@mcansh/remix-fastify");
 
+let serverBuild = require("./build/index.js");
+
 let MODE = process.env.NODE_ENV;
 
-async function start() {
-  let app = fastify();
+let app = fastify();
 
-  await app.register(remixFastifyPlugin, {
-    build: path.join(process.cwd(), "build/index.js"),
-    mode: MODE,
-    getLoadContext() {
-      return { loadContextName: "John Doe" };
-    },
-    purgeRequireCacheInDevelopment: false,
-    unstable_earlyHints: true,
-  });
-
-  let port = process.env.PORT ? Number(process.env.PORT) || 3000 : 3000;
-
-  let address = await app.listen({ port, host: "0.0.0.0" });
-  console.log(`✅ app ready: ${address}`);
-}
-
-start().catch((error) => {
-  console.error(error);
-  process.exit(1);
+await app.register(remixFastifyPlugin, {
+  build: serverBuild,
+  mode: MODE,
+  getLoadContext: () => ({ loadContextName: "John Doe" }),
+  purgeRequireCacheInDevelopment: false,
+  unstable_earlyHints: true,
 });
+
+let port = process.env.PORT ? Number(process.env.PORT) || 3000 : 3000;
+
+let address = await app.listen({ port, host: "0.0.0.0" });
+console.log(`✅ app ready: ${address}`);
+
+if (MODE === "development") {
+  let { broadcastDevReady } = await import("@remix-run/node");
+  broadcastDevReady(serverBuild);
+}
