c75ecbb4bcd79685e476da77710e564d615199b2
Logan McAnsh <logan@mcan.sh>
2023-08-01 10:41:08 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ const { globSync } = require("glob");
 
 let packages = globSync("packages/*", { absolute: true });
 
+const vitestFiles = [
+  "packages/**/__tests__/**/*",
+  "packages/**/*.{spec,test}.*",
+];
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
@@ -15,4 +20,22 @@ module.exports = {
       { packageDir: [...packages, "example", "."] },
     ],
   },
+  overrides: [
+    {
+      extends: ["@remix-run/eslint-config/jest-testing-library"],
+      files: vitestFiles,
+      rules: {
+        "testing-library/no-await-sync-events": "off",
+        "jest-dom/prefer-in-document": "off",
+      },
+      // we're using vitest which has a very similar API to jest
+      // (so the linting plugins work nicely), but it means we have to explicitly
+      // set the jest version.
+      settings: {
+        jest: {
+          version: 28,
+        },
+      },
+    },
+  ],
 };
