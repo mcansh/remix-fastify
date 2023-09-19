352928314d6b31f4589a36be4ffbd955641c9737
Logan McAnsh <logan@mcan.sh>
2023-09-19 11:52:30 -0400

    fixup! chore(playground): esm + update server with changes from example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- playground/server.js
+++ playground/server.js
@@ -8,7 +8,6 @@ import {
 } from "@mcansh/remix-fastify";
 import { installGlobals, broadcastDevReady } from "@remix-run/node";
 import sourceMapSupport from "source-map-support";
-// early hints are optional, feel free to remove this along with lines 7, 33, 47-48, and 87-88
 import { fastifyEarlyHints } from "@fastify/early-hints";
 
 sourceMapSupport.install();
@@ -44,7 +43,7 @@ app.all("*", async (request, reply) => {
   if (process.env.NODE_ENV === "development") {
     let devHandler = await createDevRequestHandler(
       initialBuild,
-      getLoadContext
+      getLoadContext,
     );
     return devHandler(request, reply);
   }
