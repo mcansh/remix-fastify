e41cbae728a7117728bc51df09385435afe9a1d2
Logan McAnsh <logan@mcan.sh>
2023-12-13 18:01:03 -0500

    chore(examples/vite): renaming of module id wasnt included
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/unstable-vite/server.js
+++ examples/unstable-vite/server.js
@@ -5,6 +5,7 @@ import { fastifyStatic } from "@fastify/static";
 import { createRequestHandler } from "@mcansh/remix-fastify";
 import { installGlobals } from "@remix-run/node";
 import { fastify } from "fastify";
+import { unstable_viteServerBuildModuleId } from "@remix-run/dev";
 
 installGlobals();
 
@@ -64,7 +65,7 @@ app.all("*", async (request, reply) => {
   try {
     let handler = createRequestHandler({
       build: vite
-        ? () => vite?.ssrLoadModule("virtual:remix/server-build")
+        ? () => vite?.ssrLoadModule(unstable_viteServerBuildModuleId)
         : await import("./build/server/index.js"),
     });
     return handler(request, reply);
