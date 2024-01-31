a95769ab2d6b8f7bbbb655f746d3cb476d6d4f0e
Logan McAnsh <logan@mcan.sh>
2024-01-31 18:11:03 -0500

    Update entry.server.tsx

--- examples/playground/app/entry.server.tsx
+++ examples/playground/app/entry.server.tsx
@@ -101,6 +101,7 @@ function handleBrowserRequest(
           const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
+          responseHsaders.set("transfer-encoding", "chunked");
 
           resolve(
             new Response(stream, {
