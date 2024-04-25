5c87dd59ae0de50e45d40a07265a34ecdcc53f1d
Logan McAnsh <logan@mcan.sh>
2024-04-24 20:33:07 -0400

    ci: update post-changeset
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ci: update post-changeset
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ci: update post-changeset
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    ci: update post-changeset
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/post-changeset.js
+++ scripts/post-changeset.js
@@ -1,10 +1,9 @@
 import fs from "node:fs";
 import path from "node:path";
-import url from "node:url";
 import { glob } from "glob";
 import PackageJson from "@npmcli/package-json";
 
-let __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let __dirname = path.dirname(new URL(import.meta.url).pathname);
 let rootDir = path.resolve(__dirname, "..");
 
 // 1. remove CHANGELOG.md from './examples/*'
@@ -12,6 +11,7 @@ let changelogs = glob.sync("./examples/*/CHANGELOG.md", { cwd: rootDir });
 
 for (let file of changelogs) {
   fs.rmSync(path.resolve(rootDir, file));
+  console.log(`✅ Removed ${file}`);
 }
 
 // 2. remove `version: null` from './examples/*/package.json'
@@ -19,9 +19,8 @@ let packageJsons = glob.sync("./examples/*/package.json", { cwd: rootDir });
 
 for (let file of packageJsons) {
   let pkg = await PackageJson.load(path.dirname(file));
-  if (pkg.content.version === null) {
+  if (!pkg.content.version) {
     delete pkg.content.version;
-    await pkg.fix({ changes: true });
     await pkg.save();
   }
 }
