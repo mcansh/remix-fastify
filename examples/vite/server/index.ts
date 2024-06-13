50020c296994a11ba604ba2477785d68745c18f9
Nowon Lee <nw.lee@outlook.com>
2024-06-14 08:18:43 +0900

    feat(examples): use Typescript in example `vite` (#310)
    * chore: use `ts-node` to run ts files
    
    * refactor: use typescript in example `vite`
    
    * fix: `pnpm-lock.yaml`
    
    * chore(vite): use plugin
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: use tsm instead of ts-node
    
    https://github.com/lukeed/tsm/issues/5#issuecomment-938921015
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    Co-authored-by: Logan McAnsh <logan@mcan.sh>

--- examples/vite/server/index.ts
+++ examples/vite/server/index.ts
@@ -1,14 +1,16 @@
+import process from "node:process";
 import { remixFastify } from "@mcansh/remix-fastify";
 import { installGlobals } from "@remix-run/node";
 import { fastify } from "fastify";
 
 installGlobals();
 
-let app = fastify();
+const app = fastify();
 
 await app.register(remixFastify);
 
-let port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 3000;
+const host = process.env.HOST === "true" ? "0.0.0.0" : "127.0.0.1";
 
-let address = await app.listen({ port, host: "0.0.0.0" });
+let address = await app.listen({ port, host });
 console.log(`✅ app ready: ${address}`);
