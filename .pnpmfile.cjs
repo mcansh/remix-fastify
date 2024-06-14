251f4815ce930531b016d06a88cf72212c5ad094
Logan McAnsh <logan@mcan.sh>
2024-06-14 07:45:19 -0400

    chore: use .pnpmfile.cjs hooks to replace local version with workspace
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .pnpmfile.cjs
@@ -0,0 +1,18 @@
+function readPackage(pkg) {
+  // replace hardcoded version of remix-fastify with workspace:*
+  // this is necessary because we're using pnpm workspaces
+  // and we want to use the local version of remix-fastify
+  // instead of the published version
+  // but we want to keep the version in the package.json
+  if (pkg.dependencies["@mcansh/remix-fastify"]) {
+    pkg.dependencies["@mcansh/remix-fastify"] = "workspace:*";
+  }
+
+  return pkg;
+}
+
+module.exports = {
+  hooks: {
+    readPackage,
+  },
+};
