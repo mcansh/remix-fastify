15b6cda808239da19dbdc5996054195d11e4042c
Logan McAnsh <logan@mcan.sh>
2024-07-20 08:19:22 -0400

    chore: make this not a resource route
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/playground/app/routes/_layout.loader-error.tsx
+++ examples/playground/app/routes/_layout.loader-error.tsx
@@ -3,3 +3,7 @@ import { unstable_defineLoader as defineLoader } from "@remix-run/node";
 export const loader = defineLoader(() => {
   throw new Error("This is an error");
 });
+
+export default function Component() {
+	return <p>You'll never see this</p>;
+}
