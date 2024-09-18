74ad0a32c1d433ecf4d167651830048bdd2ec07f
Logan McAnsh <logan@mcan.sh>
2024-09-17 21:23:50 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    chore(deps): bump to latest
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/remix-fastify/src/plugin.ts
+++ packages/remix-fastify/src/plugin.ts
@@ -164,6 +164,6 @@ export let remixFastify = fp<RemixFastifyOptions>(
   {
     // replaced with the package name during build
     name: process.env.__PACKAGE_NAME__,
-    fastify: "4.x",
-  }
+    fastify: "5.x",
+  },
 );
