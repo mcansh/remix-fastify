3ce7e27416c3da827a6dfb3604c2ba48719233f9
Logan McAnsh <logan@mcan.sh>
2024-10-15 10:08:39 -0400

    Update _layout.resource-route-error.tsx

--- examples/playground/app/routes/_layout.resource-route-error.tsx
+++ examples/playground/app/routes/_layout.resource-route-error.tsx
@@ -1,5 +1,3 @@
-import { unstable_defineLoader as defineLoader } from "@remix-run/node";
-
-export const loader = defineLoader(() => {
+export function loader() {
   throw new Error("This is an error");
-});
+}
