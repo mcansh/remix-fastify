a29c1343100a1e7c12abaf185234b50b1e573dc0
Logan McAnsh <logan@mcan.sh>
2024-11-03 12:27:45 -0500

    example: add react-router v7 example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ examples/react-router/server.js
@@ -0,0 +1,30 @@
+import chalk from "chalk";
+import { remixFastify } from "@mcansh/remix-fastify";
+import { fastify } from "fastify";
+import sourceMapSupport from "source-map-support";
+import getPort, { portNumbers } from "get-port";
+
+sourceMapSupport.install();
+
+let app = fastify();
+
+await app.register(remixFastify, {
+  virtualModule: "virtual:react-router/server-build",
+});
+
+const desiredPort = Number(process.env.PORT) || 3000;
+const portToUse = await getPort({
+  port: portNumbers(desiredPort, desiredPort + 100),
+});
+
+let address = await app.listen({ port: portToUse, host: "0.0.0.0" });
+
+if (portToUse !== desiredPort) {
+  console.warn(
+    chalk.yellow(
+      `⚠️  Port ${desiredPort} is not available, using ${portToUse} instead.`,
+    ),
+  );
+}
+
+console.log(chalk.green(`✅ app ready: ${address}`));
