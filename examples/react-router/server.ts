3fceb1c9181ae30dca38e31be283f8e7d88c69a4
Logan McAnsh <logan@mcan.sh>
2025-04-01 17:16:13 +0000

    Pending changes exported from your codespace

--- examples/react-router/server.ts
+++ examples/react-router/server.ts
@@ -2,13 +2,26 @@ import chalk from "chalk";
 import { reactRouterFastify } from "@mcansh/remix-fastify/react-router";
 import { fastify } from "fastify";
 import sourceMapSupport from "source-map-support";
+import { unstable_createContext } from "react-router";
 import getPort, { portNumbers } from "get-port";
 
 sourceMapSupport.install();
 
 let app = fastify();
 
-await app.register(reactRouterFastify);
+type AdapterContextType = {
+	session: FastifySessionObject;
+};
+
+export const adapterContext = unstable_createContext<AdapterContextType>();
+
+await app.register(reactRouterFastify, {
+	getLoadContext(request: FastifyRequest): unstable_InitialContext {
+		const map = new Map<unstable_RouterContext, unknown>();
+		map.set(adapterContext, { session: request.session });
+		return map;
+	},
+});
 
 const desiredPort = Number(process.env.PORT) || 3000;
 const portToUse = await getPort({
