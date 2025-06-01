0b9bc68372aad7d87ac483292954c5ba67877816
Logan McAnsh <logan@mcan.sh>
2025-06-01 14:56:03 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/react-router/vite.config.ts
+++ examples/react-router/vite.config.ts
@@ -1,13 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    reactRouter({
-      // Server-side render by default, to enable SPA mode set this to `false`
-      ssr: true,
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
